ebea92d8fd12164ae8bf4de7a08a553f112ef0b6
Gianluca Tranchedone <gianluca@cocoabeans.me>
2014-04-11 08:21:38 +0100

    Proper demo project created.

---
+++ Example/LVTwitterOAuthClient/LVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LVAppDelegate.h
+//  LVTwitterOAuthClient
+//
+//  Created by Gianluca Tranchedone on 11/04/2014.
+//  Copyright (c) 2014 Loovin, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
